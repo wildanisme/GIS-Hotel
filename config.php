8fc4bb852ddf1cf2117dfc9adc688f505d2a4e0d
WildanIsMe <wildantaufiq.abdulaziz@gmail.com>
2018-12-23 23:16:01 +0700

    Pemetaan Hotel di Kota Bogor
    GIS-Hotel merupakan aplikasi pemetaan hotel di kota Bogor berbasis web. Aplikasi ini merupakan remastering dari aplikasi lain.

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+
+$config["server"]='localhost';
+$config["username"]='root';
+$config["password"]='wildanisme';
+$config["database_name"]='db_gis_sederhana';
---
+++ home.php
@@ -0,0 +1,9 @@
+<div class="page-header">
+    <h1>Sistem Informasi Geografis Pemetaan Hotel Di Kota Bogor</h1>
+</div>
+<p>
+Aplikasi Sistem Indformasi Geografis Pemetaan Hotel Di Kota Bogor Berbasis Web merupakan aplikasi sederhana 
+dengan memanfaatkan teknologi Google Maps. Hal ini bertujuan untuk memudahkan pembuatan aplikasi. Mengingat, 
+pemetaan akan membutuhkan waktu lama. Pembuatan aplikasi ini bertujuan untuk memenuhi Tugas Akhir praktikum mata kuliah 
+Sistem Informasi Geografis. Juga pada praktiknya, bisa dimanfaatkan pengguna yang akan mencari Hotel di sekitar Kota Bogor.
+</p>
